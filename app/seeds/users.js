060a81b67c5e2569e9c8b0312051ded5463fb4ac
alexmoran <alex.moran4.miL@army.mil>
2024-01-03 02:57:00 -0500

    http to https

--- app/seeds/users.js
+++ app/seeds/users.js
@@ -1,12 +1,6 @@
-const bcrypt = require("bcrypt");
-
 exports.seed = function (knex) {
-  // Hash the passwords using bcrypt
-  const abbyPasswordHash = bcrypt.hashSync("loveofmylife", 10);
-  const dmariaPasswordHash = bcrypt.hashSync("2024iqmd", 10);
-
   return knex("users").insert([
-    { username: "abby", password: abbyPasswordHash, isAdmin: true },
-    { username: "dmaria", password: dmariaPasswordHash, isAdmin: false },
+    { username: "abby", password: "queensfinest", isAdmin: true },
+    { username: "dmaria", password: "2024iqm", isAdmin: false },
   ]);
 };
