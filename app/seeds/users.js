a867a44e5521ae43115eba7ec5b4255235d4bc03
alexmoran <alex.moran4.miL@army.mil>
2024-01-03 02:03:53 -0500

    ip changes

--- app/seeds/users.js
+++ app/seeds/users.js
@@ -1,5 +1,12 @@
+const bcrypt = require("bcrypt");
+
 exports.seed = function (knex) {
+  // Hash the passwords using bcrypt
+  const abbyPasswordHash = bcrypt.hashSync("loveofmylife", 10);
+  const dmariaPasswordHash = bcrypt.hashSync("2024iqmd", 10);
+
   return knex("users").insert([
-    { username: "abby", password: "loveofmylife", isAdmin: true }, // Replace with a hashed password
+    { username: "abby", password: abbyPasswordHash, isAdmin: true },
+    { username: "dmaria", password: dmariaPasswordHash, isAdmin: false },
   ]);
 };
